835082001a909788229087d3b0284fc2913e5d2b
arfiya-ansari <arfiyaansari36@gmail.com>
2025-04-28 14:57:59 +0530

    Transport

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button"
 
 export default function Navbar({ activeSection, setActiveSection }) {
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-green shadow-md">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
